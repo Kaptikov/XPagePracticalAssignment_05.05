1f3df6b4c8b71fbae9c00b8fa15cc14638f0fdf4
Kaptikov <igor.zabirow02@mail.ru>
2023-05-11 11:42:28 +0500

    Создание проекта

---
+++ src/js/app.js
@@ -0,0 +1,46 @@
+import * as flsFunctions from './modules/functions.js'
+
+flsFunctions.isWebp()
+
+// import Swiper, { Navigation } from 'swiper'
+
+// let swiper1 = new Swiper('.reviews__swiper', {
+//   modules: [Navigation],
+//   // Optional parameters
+//   loop: true,
+//   slidesPerView: 3,
+
+//   direction: 'horizontal',
+
+//   // speed: 800,
+
+//   // spaceBetween: 23,
+
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+
+//   breakpoints: {
+//     // when window width is >= 320px
+//     320: {
+//       slidesPerView: 1,
+//       spaceBetween: 44,
+//     },
+//     // when window width is >= 480px
+//     // 480: {
+//     //   slidesPerView: 2,
+//     //   spaceBetween: 30
+//     // },
+//     // when window width is >= 640px
+//     524: {
+//       slidesPerView: 2,
+//       spaceBetween: 42,
+//     },
+//     767: {
+//       slidesPerView: 3,
+//       spaceBetween: 47,
+//     },
+//   },
+// })
